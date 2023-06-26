7b3fe34d76b71742303507409b2ff7e722850c68
SakurakoHirooka <sakutoma39@docomo.ne.jp>
2023-06-26 12:02:21 +0000

    画像投稿する際、最初に出るルーティングエラーを解消した

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'd51e4bb63ba6f527145e231cfb3b3af0fffcb210ff830529f9633d082e75e6ef3fb87ae110593aa6c98d14da87803551bfb11f1302ee331b6333e52ef8fec9f8'
