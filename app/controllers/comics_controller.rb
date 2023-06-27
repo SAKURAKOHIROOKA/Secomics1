29bf4b93d657cd94e5e3c85f14679f4c738f268c
SakurakoHirooka <sakutoma39@docomo.ne.jp>
2023-06-27 02:51:11 +0000

    index, show, new, create, edit, update, destroyのアクションを定義した。ストロングパラメーターも設定した

---
+++ app/controllers/comics_controller.rb
@@ -0,0 +1,62 @@
+class ComicsController < ApplicationController
+  
+  def index
+    @user = current_user
+    
+    @comics = Comic.all
+  end
+
+  def show
+    @user = current_user
+    
+    @comic = Comic.find(params[:id])
+    @comic_user = @comic.user
+  end
+
+  def new
+    @comic = Comic.new
+  end
+  
+  def create
+    @user = current_user
+    @comics = Comic.all
+    @comic = Comic.new(comic_params)
+    @comic.user_id = current_user.id
+    if @comic.save
+       redirect_to comic_path(@comic.id), notice: 'You have created book successfully.'
+    else
+    render :index
+    end
+  end 
+
+  def edit
+    @comic = Comic.find(params[:id])
+  end
+  
+  def update
+    @comic = Comic.find(params[:id])
+    if @comic.update(comic_params)
+    redirect_to comic_path(@comic.id),notice: 'You have updated book successfully.'
+    else
+      render :edit
+    end
+  end 
+  
+  def destroy
+    comic = Comic.find(params[:id])
+    comic.destroy
+    redirect_to comics_path
+  end 
+  
+   private
+  
+  def comic_params
+    params.require(:comic).permit(:title, :story, :review, :user_id)
+  end 
+  
+  def correct_user
+    @comic= Comic.find(params[:id])
+    @user = @comic.user
+    redirect_to comics_path unless @user == current_user
+  end 
+end
